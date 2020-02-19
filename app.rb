e9818a7145c429b16ee0a005991716a8ac6fa13d
Uma Manandhar <um_2003_8@hotmail.com>
2020-02-19 00:39:23 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,4 +5,13 @@ class App < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    @puppy = params[:name], params[:breed], params[:age]
+
+  end
 end
